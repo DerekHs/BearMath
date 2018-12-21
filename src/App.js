6433fff0ad96932b50ffe294157ca20b10e45424
Derek <hsiaoderek@berkeley.edu>
2018-12-20 23:29:19 -0800

    Matrix component contains 2d array

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Matrix from './components/Matrix';
+import { List } from 'immutable';
+import { Container } from "reactstrap";
 
 class App extends Component {
   render() {
+    let m1 = new List([new List([1, 2, 3]), new List([4, 5, 6])])
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <Matrix props={{foo: "asdf"}}/>
+        <Matrix/>
+      </Container>
     );
   }
 }
