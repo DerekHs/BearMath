327b57845f3fb197f9ed0cb70d694ea13a2971f9
Derek <hsiaoderek@berkeley.edu>
2019-01-20 12:28:40 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/BearMath/precache-manifest.265310640fb9233e761b27e87d0fcb8c.js"
+  "/BearMath/precache-manifest.fb6cbbaddf20dec9fb616a600a82613d.js"
 );
 
 workbox.clientsClaim();
