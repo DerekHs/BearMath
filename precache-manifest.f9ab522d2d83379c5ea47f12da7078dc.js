592684938a622a2e38a8ebe57fc0a551f762eb6e
Derek <hsiaoderek@berkeley.edu>
2019-01-22 14:57:31 -0800

    Updates

---
+++ precache-manifest.f9ab522d2d83379c5ea47f12da7078dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd2abdafb83891ce0d57",
+    "url": "/BearMath/static/js/runtime~main.fd2abdaf.js"
+  },
+  {
+    "revision": "64605de5bee220415822",
+    "url": "/BearMath/static/js/main.64605de5.chunk.js"
+  },
+  {
+    "revision": "0d781fae92d51d58f7c8",
+    "url": "/BearMath/static/js/1.0d781fae.chunk.js"
+  },
+  {
+    "revision": "0d781fae92d51d58f7c8",
+    "url": "/BearMath/static/css/1.df4fb497.chunk.css"
+  },
+  {
+    "revision": "17200b83cca4a91c03a391632e5469ab",
+    "url": "/BearMath/index.html"
+  }
+];
