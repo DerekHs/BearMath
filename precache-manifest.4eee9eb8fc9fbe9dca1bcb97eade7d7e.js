d6560e733e7b7ae17340f42a32e444885d57f56a
DerekHs <hsiaoderek@berkeley.edu>
2022-01-27 23:08:48 -0800

    Updates

---
+++ precache-manifest.4eee9eb8fc9fbe9dca1bcb97eade7d7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "414fe038f73e3b594b05",
+    "url": "/BearMaths/static/js/runtime~main.414fe038.js"
+  },
+  {
+    "revision": "7f64e9d22056772603d0",
+    "url": "/BearMaths/static/js/main.7f64e9d2.chunk.js"
+  },
+  {
+    "revision": "6277d7b758154308994d",
+    "url": "/BearMaths/static/js/1.6277d7b7.chunk.js"
+  },
+  {
+    "revision": "6277d7b758154308994d",
+    "url": "/BearMaths/static/css/1.29123863.chunk.css"
+  },
+  {
+    "revision": "baff5811b8ff9dfeaf1cc9813056dd34",
+    "url": "/BearMaths/index.html"
+  }
+];
